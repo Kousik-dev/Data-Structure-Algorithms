66b5de65c8a1c0ace6a5d840d8e0998e5c8411f9
Kousik <kousik@Kousiks-MacBook-Air.local>
2024-05-31 21:28:57 +0530

    Day 2

---
+++ 1.Sorting/Merge.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+void combine(int &a, int low, int mid, int high){
+    int temp[high+1];
+    int k = 0;
+    while(low<=mid+1 and mid)
+
+}
+int mergesort(int &a, int low , int high)
+{
+    int mid = (low + high)/2;
+    if(low == high)
+    return ;
+    mergesort(a, low, mid+1);
+    mergesort(a, mid+1, high);
+    combine(a, low, mid, high)
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i = 0; i<n; i++)
+    cin>>a[i];
+    cout<<"Before Sorting .....";
+    for(int val:a)
+    cout<<val<" ";
+    cout<<endl;
+    int low = 0, high = n-1;
+    mergesort(a, low, high);
+}
