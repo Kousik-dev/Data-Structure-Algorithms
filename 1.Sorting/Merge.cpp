b424a075b24a4af65a17bd091c0d50e559a12076
Kousik <kousik@Kousiks-MacBook-Air.local>
2024-06-26 23:20:33 +0530

    Merge Sort

--- 1.Sorting/Merge.cpp
+++ 1.Sorting/Merge.cpp
@@ -1,31 +1,57 @@
 #include<bits/stdc++.h>
 using namespace std;
-void combine(int &a, int low, int mid, int high){
-    int temp[high+1];
-    int k = 0;
-    while(low<=mid+1 and mid)
+void combine(vector<int> &a, int low, int mid, int high){
+    vector<int>temp;
+    int left = low, right = mid+1;
+    while(left<= mid and right<=high){
+        if(a[left]<=a[right])
+        {
+            temp.push_back(a[left]);
+            left++;
+        }
+        else{
+            temp.push_back(a[right]);
+            right++;
+        }
+    }
+    while(left<=mid){
+        temp.push_back(a[left]);
+        left++;
+    }
+    while(right<=high){
+        temp.push_back(a[right]);
+        right++;
+    }
+
+    for(int i = low ; i<=high; i++){
+        a[i] = temp[i-low];
+    }
+    
 
 }
-int mergesort(int a[], int low , int high)
+void mergesort(vector<int>&a, int low , int high)
 {
+    if(low >= high)
+    return;
     int mid = (low + high)/2;
-    if(low == high)
-    return ;
-    mergesort(a, low, mid+1);
+    mergesort(a, low , mid);
     mergesort(a, mid+1, high);
-    combine(a, low, mid, high);
+    combine(a,low, mid, high);
 }
 int main()
 {
     int n;
     cin>>n;
-    int a[n];
+    vector<int>a(n);
     for(int i = 0; i<n; i++)
     cin>>a[i];
-    cout<<"Before Sorting .....";
+    cout<<"Before Sorting"<<endl;
     for(int val:a)
     cout<<val<<" ";
     cout<<endl;
     int low = 0, high = n-1;
     mergesort(a, low, high);
+    cout<<"After Sorting"<<endl;
+    for(int val:a)
+    cout<<val<<" ";
 }
